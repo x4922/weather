9f4b9aee6f819e25f31848c679b4736f92349c92
Shirwin <x4922@hotmail.com>
2016-02-25 13:29:58 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/db/WeatherOpenHelper.java
@@ -0,0 +1,43 @@
+package db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WeatherOpenHelper extends SQLiteOpenHelper{
+//	创建省级的表
+	private static final String CREATE_SHEN="create table Province ("
+			+ "id integer primary key autoincrement,"
+			+ "province_name text,"
+			+ "province_code text)";
+	//创建市级表
+	private static final String CREATE_CITY="create table city ("
+			+ "id integer primary key autoincrement,"
+			+ "city_name text,"
+			+ "city_code text,"
+			+ "province_id integer)";
+	//创建县级表
+	private static final String CREATE_COUNTY="create table city ("
+			+ "id interger primary key autoincrement,"
+			+ "county_name text,"
+			+ "county_code text,"
+			+ "city_id integer)";
+	public WeatherOpenHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO 自动生成的构造函数存根
+	}
+	@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO 自动生成的方法存根
+			db.execSQL(CREATE_SHEN);
+			db.execSQL(CREATE_CITY);
+			db.execSQL(CREATE_COUNTY);
+		}
+	@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO 自动生成的方法存根
+			
+		}
+
+}
---
+++ src/model/WeatherDB.java
@@ -0,0 +1,111 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+import org.apache.http.client.fluent.Content;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import db.WeatherOpenHelper;
+
+public class WeatherDB {
+	//数据库名称
+	public static final String DB_NAME="Weathe";
+	//数据库版本
+	public static final int VERSION=1;
+	
+	private static WeatherDB weatherDB;
+	private SQLiteDatabase db;
+	// 构造WeatherDB私有方法
+	private WeatherDB(Context context) {
+		// TODO 自动生成的构造函数存根
+		WeatherOpenHelper dbHelper=new WeatherOpenHelper(context, DB_NAME, null, VERSION);
+		db=dbHelper.getReadableDatabase();
+	}
+	//获取实例
+	public synchronized static WeatherDB getInstance(Context context) {
+		if(weatherDB==null){
+			weatherDB= new WeatherDB(context);
+		}
+		return weatherDB;
+	}
+	//将实例储存
+	//省 Province 
+	public void saveProvince(Province province){
+		if(province != null){
+			ContentValues values=new ContentValues();
+			values.put("province_name", province.getProvinceName());
+			values.put("province_code", province.getProvinceCode());
+			db.insert("Provunce", null, values);
+		}
+	}
+	//市 city
+	public void saveCity(City city){
+		if(city != null){
+			ContentValues values=new ContentValues();
+			values.put("province_name", city.getCityName());
+			values.put("province_code", city.getCityCode());
+			values.put("provunce_id", city.getProvinceId());
+			db.insert("City", null, values);
+		}
+	}
+	//县区 county 
+	public void saveCounty(County county){
+		if(county != null){
+			ContentValues values=new ContentValues();
+			values.put("province_name", county.getCountyName());
+			values.put("province_code", county.getCountyCode());
+			values.put("provunce_id", county.getCityId());
+			db.insert("County", null, values);
+		}
+	}
+    //从数据库读取信息
+	//省 Province
+	public List<Province> loadProvince(){
+		List<Province> list=new ArrayList<Province>();
+		Cursor cursor=db.query("Province", null, null, null, null, null, null);
+		if(cursor.moveToFirst()){
+			do{
+				Province province=new Province();
+				province.setId(cursor.getColumnIndex("id"));
+				province.setProvinceName(cursor.getString(cursor.getColumnIndex("province_name")));
+				province.setProvinceCode(cursor.getString(cursor.getColumnIndex("province_code")));
+			} while(cursor.moveToNext());
+		}
+		return list;
+	}
+	//市 City
+	public List<City> loadCity(){
+		List<City> list=new ArrayList<City>();
+		Cursor cursor=db.query("City", null, null, null, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				City city=new City();
+				city.setId(cursor.getColumnIndex("id"));
+				city.setCityName(cursor.getString(cursor.getColumnIndex("city_name")));
+				city.setCityCode(cursor.getString(cursor.getColumnIndex("city_code")));
+				city.setProvinceId(cursor.getColumnIndex("provunce_id"));
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
+	//县区 county
+	public List<County> loadCounty(){
+		List<County> list=new ArrayList<County>();
+		Cursor cursor=db.query("County", null, null, null, null, null, null);
+		if (cursor.moveToFirst()) {
+			do {
+				County county=new County();
+				county.setId(cursor.getColumnIndex("id"));
+				county.setCountyName(cursor.getString(cursor.getColumnIndex("province_name")));
+				county.setCountyCode(cursor.getString(cursor.getColumnIndex("province_code")));
+				county.setCityId(cursor.getColumnIndex("city_id"));
+			} while (cursor.moveToNext());
+		}
+		return list;
+	}
+}
